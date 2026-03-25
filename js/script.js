905196f531f1a603890c838a0a5142d8efae4a89
rafa1803 <maurybionico18@gmail.com>
2026-03-25 13:14:30 -0300

    Creación de tarjetas para section servicios y planes, junto con su interacción con las mismas

--- js/script.js
+++ js/script.js
@@ -32,3 +32,54 @@ btn.addEventListener("click", ()=>{
         btn.textContent = "Ver más";
     }
 });
+
+const cards = document.querySelectorAll(".card");
+// const values = document.querySelectorAll(".value");
+
+cards.forEach(card => {
+    const cardHeader = card.querySelector(".card-header");
+    const content = card.querySelector(".card-content");
+    
+    cardHeader.addEventListener("click", () => {
+        // e.stopPropagation();
+
+        cards.forEach(c => {
+            if (c !== card){
+                c.classList.remove("active");
+                c.querySelector(".card-content").style.maxHeight = null;
+            }
+        });
+        card.classList.toggle("active");
+
+        if (card.classList.contains("active")){
+            content.style.maxHeight = content.scrollHeight + "px";
+        }else {
+            content.style.maxHeight = null;
+        }
+    });
+});
+
+const servicios = document.querySelectorAll(".servicio");
+
+servicios.forEach(servicio => {
+    const servHeader = servicio.querySelector(".servicio-header");
+    const servContent = servicio.querySelector(".servicio-content");
+    
+    servHeader.addEventListener("click", () => {
+        // e.stopPropagation();
+
+        servicios.forEach(s => {
+            if (s !== servicio){
+                s.classList.remove("active");
+                s.querySelector(".servicio-content").style.maxHeight = null;
+            }
+        });
+        servicio.classList.toggle("active");
+
+        if (servicio.classList.contains("active")){
+            servContent.style.maxHeight = servContent.scrollHeight + "px";
+        }else {
+            servContent.style.maxHeight = null;
+        }
+    });
+});
